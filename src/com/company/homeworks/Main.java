fa20eb725b1eac59c7f94370f909bbbe7fda917a
Viktor Yeromenko <hanom94@gmail.com>
2019-04-10 18:29:46 +0300

    123

--- src/com/company/homeworks/Main.java
+++ src/com/company/homeworks/Main.java
@@ -3,7 +3,7 @@ package com.company.homeworks;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// write your code herehjh
         System.out.println("Hello!!!!!");
     }
 }
